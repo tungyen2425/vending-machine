cc2fadbac875a9f66f8097dccea7e24581181167
Tung Nguyen <tung25125@gmail.com>
2025-04-17 13:25:23 +0700

    Add VCS configuration and update database connection URL

--- src/main/java/com/vendingmachine/database/DatabaseConnection.java
+++ src/main/java/com/vendingmachine/database/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/vending_machine?useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/vending_machine?useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";     
     private static final String PASSWORD = "25122005";     
 
